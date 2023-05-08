dd4106c810ff9d42febfec89b085aea76e91c298
Stefan Schlicker <stefanschlicker@posteo.de>
2023-05-08 11:46:32 +0200

    initial setup

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define(
+  ["./BaseController", "sap/m/MessageBox"],
+  function (BaseController, MessageBox) {
+    "use strict";
+
+    return BaseController.extend(
+      "sls.validation.emptyfieldcheck.controller.Main",
+      {}
+    );
+  }
+);
