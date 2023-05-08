cbe82768eccfcf253372aca7a7ba29ca2e328316
Stefan Schlicker <stefanschlicker@posteo.de>
2023-05-08 12:33:23 +0200

    add input and controller logic

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,24 @@
-sap.ui.define(
-  ["./BaseController", "sap/m/MessageBox"],
-  function (BaseController, MessageBox) {
-    "use strict";
+sap.ui.define(["./BaseController"], function (BaseController) {
+  "use strict";
 
-    return BaseController.extend(
-      "sls.validation.emptyfieldcheck.controller.Main",
-      {}
-    );
-  }
-);
+  return BaseController.extend(
+    "sls.validation.emptyfieldcheck.controller.Main",
+    {
+      validateInputs: function (oEvent) {
+        // this function gets the first textfield
+        const emailTextField = this.getView().byId("email");
+
+        //this function checks its value, you can insert more checks on the value
+        if (emailTextField.getValue() === "") {
+          alert("Please enter an email adress.");
+        }
+
+        const nameTextField = this.getView().byId("name");
+
+        if (nameTextField.getValue() === "") {
+          alert("Please enter a name.");
+        }
+      },
+    }
+  );
+});
