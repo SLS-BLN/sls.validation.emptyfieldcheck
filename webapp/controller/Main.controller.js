f2756b66e26cef427b7c5a3498e5db9669c41ccf
Stefan Schlicker <stefanschlicker@posteo.de>
2023-05-09 15:51:56 +0200

    testing

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,24 +1,41 @@
-sap.ui.define(["./BaseController"], function (BaseController) {
-  "use strict";
+sap.ui.define(
+  ["./BaseController", "sap/ui/model/json/JSONModel"],
+  function (BaseController, JSONModel) {
+    "use strict";
 
-  return BaseController.extend(
-    "sls.validation.emptyfieldcheck.controller.Main",
-    {
-      validateInputs: function (oEvent) {
-        // this function gets the first textfield
-        const emailTextField = this.getView().byId("email");
+    return BaseController.extend(
+      "sls.validation.emptyfieldcheck.controller.Main",
+      {
+        onInit: function () {
+          const oModel = new JSONModel({
+            plz: "",
+          });
 
-        //this function checks its value, you can insert more checks on the value
-        if (emailTextField.getValue() === "") {
-          alert("Please enter an email adress.");
-        }
+          this.getView().setModel(oModel);
+        },
 
-        const nameTextField = this.getView().byId("name");
+        validateInputs: function (oEvent) {
+          // this function gets the first textfield
+          const emailTextField = this.getView().byId("email");
 
-        if (nameTextField.getValue() === "") {
-          alert("Please enter a name.");
-        }
-      },
-    }
-  );
-});
+          //this function checks its value, you can insert more checks on the value
+          if (emailTextField.getValue() === "") {
+            alert("Please enter an email adress.");
+          }
+
+          const nameTextField = this.getView().byId("name");
+
+          if (nameTextField.getValue() === "") {
+            alert("Please enter a name.");
+          }
+        },
+
+        onChange: function () {
+          // if (this.getValue() === "")
+          //   this.setValueState(sap.ui.core.ValueState.Error);
+          // else this.setValueState(sap.ui.core.ValueState.Success);
+        },
+      }
+    );
+  }
+);
