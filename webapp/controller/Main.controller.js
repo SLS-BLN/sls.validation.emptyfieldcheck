77572fe4e995adec1de9116d4759020cd53a632b
Stefan Schlicker <stefanschlicker@posteo.de>
2023-05-21 12:04:35 +0200

    upd onSubmit - test all fields

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -28,12 +28,6 @@ sap.ui.define(
     return BaseController.extend(
       "sls.validation.emptyfieldcheck.controller.Main",
       {
-        types: {
-          password: new PasswordType(),
-          dateTime: new DateTimeType(),
-          email: new EmailType(),
-        },
-
         onInit: function () {
           const oView = this.getView();
           const oMM = Core.getMessageManager();
@@ -56,21 +50,10 @@ sap.ui.define(
           // oMM.registerObject(oView.byId("dateTimeInput"), true);
         },
 
-        _validateInput: function (oInput) {
-          let sValueState = "None";
-          let bValidationError = false;
-          const oBinding = oInput.getBinding("value");
-
-          try {
-            oBinding.getType().validateValue(oInput.getValue());
-          } catch (oException) {
-            sValueState = "Error";
-            bValidationError = true;
-          }
-
-          oInput.setValueState(sValueState);
-
-          return bValidationError;
+        types: {
+          password: new PasswordType(),
+          dateTime: new DateTimeType(),
+          email: new EmailType(),
         },
 
         onNameLiveChange: function (oEvent) {
@@ -86,6 +69,8 @@ sap.ui.define(
             oView.byId("nameInput"),
             oView.byId("emailInput"),
             oView.byId("ageInput"),
+            oView.byId("passwordInput"),
+            oView.byId("dateTimeInput"),
           ];
           let bValidationError = false;
 
@@ -96,19 +81,32 @@ sap.ui.define(
           }, this);
 
           if (!bValidationError) {
-            // eslint-disable-next-line no-warning-comments
-            // TODO: i18n
             const msg = "The input is validated. Your form has been submitted.";
 
             MessageToast.show(msg);
           } else {
-            // eslint-disable-next-line no-warning-comments
-            // TODO: i18n
             const msg =
               "A validation error has occurred. Complete your input first.";
 
             MessageBox.alert(msg);
           }
+        },
+
+        _validateInput: function (oInput) {
+          let sValueState = "None";
+          let bValidationError = false;
+          const oBinding = oInput.getBinding("value");
+
+          try {
+            oBinding.getType().validateValue(oInput.getValue());
+          } catch (oException) {
+            sValueState = "Error";
+            bValidationError = true;
+          }
+
+          oInput.setValueState(sValueState);
+
+          return bValidationError;
         },
       }
     );
